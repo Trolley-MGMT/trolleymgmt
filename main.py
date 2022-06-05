10b3a073274af78a978349c97bb3cbf6da7aa545
Lior Yardeni <Liorwhy@gmail.com>
2022-06-05 14:35:51 +0300

    more user name fixes

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ logger.info(f'The content of the directory is: {os.listdir(CUR_DIR)}')
 def user_registration(first_name: str = '', last_name: str = '', password: str = '',
                       user_email: str = '', team_name: str = '') -> bool:
     """"""
-    user_name = f'{first_name.lower()}_{last_name.lower()}'
+    user_name = f'{first_name.lower()}.{last_name.lower()}'
     hashed_password = generate_password_hash(password, method='sha256')
     user_object = UserObject(first_name=first_name, last_name=last_name, user_name=user_name, user_email=user_email,
                              team_name=team_name, hashed_password=hashed_password)
