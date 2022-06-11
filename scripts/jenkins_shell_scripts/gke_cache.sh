0671a8c329c41d252832a80da6154d5659bfa497
Lior Yardeni <Liorwhy@gmail.com>
2022-06-11 16:29:19 +0300

    fixed gke_cache.sh

--- scripts/jenkins_shell_scripts/gke_cache.sh
+++ scripts/jenkins_shell_scripts/gke_cache.sh
@@ -23,7 +23,7 @@ echo "Installing projects requirements"
 pip3 install -r $WORKSPACE/requirements.txt
 
 # Running gke_cache script
-echo "Running python3 $WORKSPACE/scripts/jenkins_shell_scripts/gke_caching_script.py"
-python3 $WORKSPACE/scripts/jenkins_shell_scripts/gke_caching_script.py
+echo "Running python3 $WORKSPACE/scripts/gke_caching_script.py"
+python3 $WORKSPACE/scripts/gke_caching_script.py
 
 rm -R $WORKSPACE/$RANDOM_VENV
