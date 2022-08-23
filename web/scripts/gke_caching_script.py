eb48f47ff8fe1620a43e79923a7bdcf2347fd169
Pavel <zagalsky@gmail.com>
2022-08-23 09:35:24 +0300

    adding recent changes

--- web/scripts/gke_caching_script.py
+++ web/scripts/gke_caching_script.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import platform
 import json
 
@@ -134,6 +135,11 @@ def main():
 
 if __name__ == '__main__':
     command = '/usr/lib/google-cloud-sdk/bin/gcloud compute zones list'
+    google_creds_json_path = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+    print(f'This is the path for google creds file: {google_creds_json_path}')
+    with open(google_creds_json_path, "r") as f:
+        google_creds = f.read()
+        print(f'The google_creds content is: {google_creds}')
     result = run(command, stdout=PIPE, stderr=PIPE, text=True, shell=True)
     print(f'Checking the command {result}')
     main()
