e20f4977d1cae833a2e2905ee87ce184e436ecd3
Pavel <zagalsky@gmail.com>
2022-08-27 11:32:35 +0300

    some more

--- deployment_scripts/runner.sh
+++ deployment_scripts/runner.sh
@@ -3,5 +3,5 @@ export PYTHONPATH=/home/pavelzagalsky/trolley
 export SECRET_KEY=s3cr3tk3y
 which python3
 source /home/pavelzagalsky/venv/bin/activate
-cd /home/pavelzagalsky/trolley || exit
+cd /home/pavelzagalsky/trolley/web || exit
 python3 main.py &
