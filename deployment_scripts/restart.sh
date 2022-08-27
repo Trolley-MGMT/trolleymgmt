80da4704d6a69ed6fa828609818b12d35679768b
Pavel <zagalsky@gmail.com>
2022-08-27 11:34:19 +0300

    some more

--- deployment_scripts/restart.sh
+++ deployment_scripts/restart.sh
@@ -3,6 +3,6 @@ kill $(lsof -i:8081 | awk '{print $2}'| awk 'NR==2{print $1}')
 export PYTHONPATH=/home/pavelzagalsky/trolley
 export SECRET_KEY=s3cr3tk3y
 which python3
-source /home/pavelzagalsky/venv/bin/activate
+source /home/pavelzagalsky/trolley_venv/bin/activate
 cd /home/pavelzagalsky/trolley/web || exit
 python3 main.py &
--- deployment_scripts/runner.sh
+++ deployment_scripts/runner.sh
@@ -2,6 +2,6 @@
 export PYTHONPATH=/home/pavelzagalsky/trolley
 export SECRET_KEY=s3cr3tk3y
 which python3
-source /home/pavelzagalsky/venv/bin/activate
+source /home/pavelzagalsky/trolley_venv/bin/activate
 cd /home/pavelzagalsky/trolley/web || exit
 python3 main.py &
