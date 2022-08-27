19667e96a75c7d9c51594989c655d447f83be495
Pavel <zagalsky@gmail.com>
2022-08-27 11:31:21 +0300

    some more

--- deployment_scripts/restart.sh
+++ deployment_scripts/restart.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 kill $(lsof -i:8081 | awk '{print $2}'| awk 'NR==2{print $1}')
-export PYTHONPATH=/home/lioryardeni/trolley
+export PYTHONPATH=/home/pavelzagalsky/trolley
 export SECRET_KEY=s3cr3tk3y
 which python3
-source /home/lioryardeni/venv/bin/activate
-cd /home/lioryardeni/trolley || exit
+source /home/pavelzagalsky/venv/bin/activate
+cd /home/pavelzagalsky/trolley || exit
 python3 main.py &
