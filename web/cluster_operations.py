c1e2731091c3c07208fe276747f2196cf27a4d34
Pavel <zagalsky@gmail.com>
2022-08-28 22:57:12 +0300

    changed github actions os env

--- web/cluster_operations.py
+++ web/cluster_operations.py
@@ -6,7 +6,7 @@ from mongo_handler.mongo_utils import retrieve_cluster_details
 from utils import random_string
 from variables.variables import GKE, ZONE_NAME, EKS, REGION_NAME
 
-GITHUB_ACTION_TOKEN = os.getenv('GITHUB_ACTION_TOKEN')
+GITHUB_ACTION_TOKEN = os.getenv('ACTION_TOKEN')
 GITHUB_ACTIONS_API_URL = 'https://api.github.com/repos/LiorYardeni/trolley/dispatches'
 GITHUB_ACTION_REQUEST_HEADER = {
     'Content-type': 'application/json',
