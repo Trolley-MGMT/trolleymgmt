eb6d2f70cc182fb003a9551ac6ada18cd05fa9e6
Einat Soferman <einatsof@gmail.com>
2022-09-15 09:48:07 +0300

    Extend clusters management to all clusters types

--- web/front/src/components/showClusters.jsx
+++ web/front/src/components/showClusters.jsx
@@ -7,9 +7,10 @@ class ShowClusters extends Component {
     super(props);
     const { trolleyRemoteUrl, trolleyLocalUrl, debug, userName } = this.props.appData;
     this.state = {
+      loading: true,
       trolleyUrl: debug ? trolleyLocalUrl : trolleyRemoteUrl,
       port: 8081,
-      clusterType: 'aks',
+      clusterType: this.props.type,
       data: [],
       userName: userName
     }
@@ -18,6 +19,7 @@ class ShowClusters extends Component {
   async componentDidMount() {
     const { trolleyUrl, port, clusterType, userName } = this.state;
     let url = `http://${trolleyUrl}:${port}/get_clusters_data?cluster_type=${clusterType}&user_name=${userName}`;
+    //let url = `http://${trolleyUrl}:${port}/get_clusters_data?cluster_type=${clusterType}&user_name=einatsoferman`;
     try {
       const response = await fetch(url);
       if (!response.ok){
@@ -26,29 +28,35 @@ class ShowClusters extends Component {
       }
       const data = await response.json();
       console.log(data);
-      this.setState({ data });
+      this.setState({ data, loading: false });
     } catch(error) {
       throw new Error(error);
     }
   }
 
   renderTable() {
-    if (this.state.data.length === 0){
-      return <p>No clusters to show, Make a new one.</p>
+    if (this.state.loading){
+      return <div className="text-center mt-5"><div className="spinner-border"></div></div>;
     }
-    return (
-      <div className="table-responsive">
-        <Table
-          data={this.state.data} />
-      </div>
-    );
+    else {
+      if (this.state.data.length === 0){
+        const link = `/build-${this.props.type}-clusters`;
+        return <p>No clusters to show, <a href={link}>Make a new one</a>.</p>;
+      }
+      return (
+        <div className="table-responsive">
+          <Table
+            data={this.state.data} />
+        </div>
+      );
+      }
   }
   
   render() {
     return(
       <div className="col-lg-10 col-8 text-center">
         <br />
-        <h2>Manage AKS clusters</h2>
+        <h2>Manage {this.state.clusterType.toUpperCase()} clusters</h2>
         <br />
         {this.renderTable()}
       </div>
