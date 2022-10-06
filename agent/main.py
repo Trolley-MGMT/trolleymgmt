99123f2af14e18b6f82dca9892b75cacf6b53235
Pavel <zagalsky@gmail.com>
2022-10-06 09:55:47 +0300

    more fixes for logging

--- agent/main.py
+++ agent/main.py
@@ -23,7 +23,7 @@ logger = logging.getLogger()
 # else:
 log_path = '/var/log/'
 file_name = 'agent_main.log'
-fileHandler = logging.FileHandler("{0}/{1}".format(log_path, file_name))
+fileHandler = logging.FileHandler(f"{log_path}/{file_name}")
 fileHandler.setFormatter(logFormatter)
 logger.addHandler(fileHandler)
 
