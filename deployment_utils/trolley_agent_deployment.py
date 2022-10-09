8fb904b2251be716023544978331a2efa336eb17
Pavel <zagalsky@gmail.com>
2022-10-09 18:44:05 +0300

    added agent deployment logic

--- deployment_utils/trolley_agent_deployment.py
+++ deployment_utils/trolley_agent_deployment.py
@@ -19,7 +19,11 @@ MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', 'password')
 MONGO_URL = os.environ.get('MONGO_URL', 'localhost')
 KUBECONFIG_PATH = os.environ.get('KUBECONFIG_PATH', '/home/runner/.kube/config')
 
-log_path = f'{os.getcwd()}'
+if 'macOS' in platform.platform():
+    log_path = f'{os.getcwd()}'
+else:
+    log_path = '/home/runner/work/Trolley-Management/Trolley-Management'
+
 log_file_name = 'agent_main.log'
 trolley_deployment_path = f'{os.getcwd()}'
 deployment_yaml_path_ = "/".join(trolley_deployment_path.split("/")[:-1])
