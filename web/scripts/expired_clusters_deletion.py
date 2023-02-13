506cba0aa65bc96b8c7e8d39f66f2a98a3df645c
Pavel <zagalsky@gmail.com>
2023-02-13 15:05:31 +0200

    refactoring for the agent deployment

--- web/scripts/expired_clusters_deletion.py
+++ web/scripts/expired_clusters_deletion.py
@@ -7,7 +7,9 @@ from web.cluster_operations import delete_gke_cluster
 
 def delete_clusters(cluster_type: str):
     expired_clusters_list = retrieve_expired_clusters(cluster_type=cluster_type)
+    print(f'clusters to expire: {expired_clusters_list}')
     for expired_cluster in expired_clusters_list:
+        print(f'expiring {expired_clusters_list} cluster')
         delete_gke_cluster(cluster_name=expired_cluster['cluster_name'])
         time.sleep(5)
         set_cluster_availability(cluster_type=cluster_type, cluster_name=expired_cluster['cluster_name'],
