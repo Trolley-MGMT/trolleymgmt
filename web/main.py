eda5b9ae014f54def5785a944db6a7ea161748c3
Pavel <zagalsky@gmail.com>
2023-05-08 11:56:02 +0300

    working on saas demo

--- web/main.py
+++ web/main.py
@@ -22,20 +22,20 @@ from werkzeug.datastructures import FileStorage
 from werkzeug.security import generate_password_hash, check_password_hash
 
 if 'Darwin' in platform.system():
-    import mongo_handler.mongo_utils
-    from mongo_handler.mongo_objects import UserObject, DeploymentYAMLObject
-    from variables.variables import POST, GET, EKS, \
+    import web.mongo_handler.mongo_utils
+    from web.mongo_handler.mongo_objects import UserObject, DeploymentYAMLObject
+    from web.variables.variables import POST, GET, EKS, \
         APPLICATION_JSON, CLUSTER_TYPE, GKE, AKS, DELETE, USER_NAME, REGIONS_LIST, \
         ZONES_LIST, HELM_INSTALLS_LIST, GKE_VERSIONS_LIST, GKE_IMAGE_TYPES, HELM, LOCATIONS_DICT, \
         CLUSTER_NAME, AWS, PROVIDER, GCP, AZ, PUT, OK, FAILURE, OBJECT_TYPE, CLUSTER, INSTANCE, TEAM_NAME, ZONE_NAMES, \
         NAMES, REGION_NAME, CLIENT_NAME
-    from cluster_operations import trigger_gke_build_github_action, trigger_eks_build_github_action, \
+    from web.cluster_operations import trigger_gke_build_github_action, trigger_eks_build_github_action, \
         trigger_aks_build_github_action, delete_gke_cluster, delete_eks_cluster, delete_aks_cluster, \
         trigger_trolley_agent_deployment_github_action
     from mail_handler import MailSender
-    from utils import random_string, apply_yaml
-    from mongo_handler.mongo_objects import ProviderObject
-    from scripts import gcp_discovery_script, aws_discovery_script
+    from web.utils import random_string, apply_yaml
+    from web.mongo_handler.mongo_objects import ProviderObject
+    from web.scripts import gcp_discovery_script, aws_discovery_script
 else:
     from mongo_handler.mongo_objects import UserObject, DeploymentYAMLObject
     from variables.variables import POST, GET, EKS, \
@@ -282,7 +282,7 @@ def get_instances_data():
     provider_type = request.args.get(PROVIDER)
     client_name = request.args.get(CLIENT_NAME.lower())
     user_name = request.args.get(USER_NAME.lower())
-    instances_list = mongo_handler.mongo_utils.retrieve_instances(provider_type, client_name, user_name)
+    instances_list = mongo_utils.retrieve_instances(provider_type, client_name, user_name)
     return Response(json.dumps(instances_list), status=200, mimetype=APPLICATION_JSON)
 
 
