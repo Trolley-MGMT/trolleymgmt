56677fbb43fbd9937b1b053c5d6787f1deee7abe
Pavel <zagalsky@gmail.com>
2023-06-05 15:08:25 +0300

    trying to push many parameters into github actions

--- deployment_utils/json_converter.py
+++ deployment_utils/json_converter.py
@@ -1,4 +1,5 @@
 import ast
+import os
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 
@@ -7,8 +8,26 @@ def main(incoming_string: str = ''):
     encoded_content = ast.literal_eval(incoming_string)
     gcp_project_id = encoded_content['gcp_project_id']
     cluster_name = encoded_content['cluster_name']
-    print(gcp_project_id)
-    print(cluster_name)
+    user_name = encoded_content['user_name']
+    cluster_version = encoded_content['cluster_version']
+    gke_machine_type = encoded_content['gke_machine_type']
+    region_name = encoded_content['region_name']
+    zone_name = encoded_content['zone_name']
+    image_type = encoded_content['image_type']
+    num_nodes = encoded_content['num_nodes']
+    expiration_time = encoded_content['expiration_time']
+    print(f'gcp_project_id is: {gcp_project_id}')
+    print(f'cluster_name is: {cluster_name}')
+    print(f'user_name is: {user_name}')
+    print(f'cluster_version is: {cluster_version}')
+    print(f'gke_machine_type is: {gke_machine_type}')
+    print(f'region_name is: {region_name}')
+    print(f'zone_name is: {zone_name}')
+    print(f'image_type is: {image_type}')
+    print(f'num_nodes is: {num_nodes}')
+    print(f'expiration_time is: {expiration_time}')
+
+    os.environ["GCP_PROJECT_ID"] = gcp_project_id
 
 
 if __name__ == '__main__':
