09aabd462983a705bdccaa8fe07f3757a2e39c86
Pavel <zagalsky@gmail.com>
2023-06-23 09:32:22 +0300

    changed debug level

--- web/main.py
+++ web/main.py
@@ -39,7 +39,7 @@ handlers = [file_handler, stdout_handler]
 
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
     handlers=handlers
 )
