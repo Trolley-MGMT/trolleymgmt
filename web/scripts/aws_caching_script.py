e3fce8ab80831730dd171e7440c7fe30b943930a
Pavel <zagalsky@gmail.com>
2023-06-23 09:27:56 +0300

    discovery fix

--- web/scripts/aws_caching_script.py
+++ web/scripts/aws_caching_script.py
@@ -28,7 +28,7 @@ stdout_handler = logging.StreamHandler(stream=sys.stdout)
 handlers = [file_handler, stdout_handler]
 
 logging.basicConfig(
-    level=logging.WARNING,
+    level=logging.DEBUG,
     format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
     handlers=handlers
 )
--- web/scripts/gcp_caching_script.py
+++ web/scripts/gcp_caching_script.py
@@ -27,7 +27,7 @@ stdout_handler = logging.StreamHandler(stream=sys.stdout)
 handlers = [file_handler, stdout_handler]
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
     handlers=handlers
 )
