149619b141a0e8160e0b86bf92a508c6662dc365
Pavel <zagalsky@gmail.com>
2023-09-11 13:46:09 +0300

    added gcp tests fixes

--- web/scripts/tests/test_gcp_caching_script.py
+++ web/scripts/tests/test_gcp_caching_script.py
@@ -6,6 +6,9 @@ import platform
 import getpass as gt
 import sys
 
+from google.cloud.compute import ZonesClient
+from google.oauth2 import service_account
+from googleapiclient import discovery
 
 from dotenv import load_dotenv
 
@@ -13,10 +16,9 @@ project_folder_ = os.path.expanduser(os.getcwd())
 project_folder = "/".join(project_folder_.split('/')[:-2])
 load_dotenv(os.path.join(project_folder, '.env'))
 
-from web.scripts.gcp_caching_script import fetch_zones, fetch_regions
+from web.scripts.gcp_caching_script import fetch_zones, fetch_regions, fetch_gke_image_types, fetch_kubernetes_versions
 
 LOCAL_USER = gt.getuser()
-GITHUB_ACTIONS_ENV = os.getenv('GITHUB_ACTIONS_ENV')
 
 log_file_name = 'server.log'
 log_file_path = f'{os.getcwd()}/{log_file_name}'
@@ -34,26 +36,13 @@ logging.basicConfig(
 )
 
 if 'Darwin' in platform.system():
-    logger.info(f'platform system is Darwin')
     GCP_CREDENTIALS_DIRECTORY = f'/Users/{LOCAL_USER}/.gcp'
-    GCP_CREDENTIALS_PATH = f'{GCP_CREDENTIALS_DIRECTORY}/gcp_credentials.json'
 else:
-    if GITHUB_ACTIONS_ENV:
-        GCP_CREDENTIALS_DIRECTORY = "/home/runner/work"
-        GCP_CREDENTIALS_PATH = f'{GCP_CREDENTIALS_DIRECTORY}/gcp_credentials.json'
-    else:
-        logger.info(f'checking the docker_env path')
-        GCP_CREDENTIALS_DIRECTORY = "/home/app/.gcp"
-        GCP_CREDENTIALS_PATH = f'{GCP_CREDENTIALS_DIRECTORY}/gcp_credentials.json'
+    GCP_CREDENTIALS_DIRECTORY = "/home/runner/work"
+GCP_CREDENTIALS_PATH = f'{GCP_CREDENTIALS_DIRECTORY}/gcp_credentials.json'
 
 
 def get_gcp_credentials() -> str:
-    # try:
-    #     google_creds_json = os.getenv(GOOGLE_CREDS_JSON)
-    #     return google_creds_json
-    # except Exception as e:
-    #     logger.warning(f'google_creds_json was not passed: {e}')
-    # if 'Darwin' in platform.system():
     try:
         with open(GCP_CREDENTIALS_PATH, "r") as f:
             google_creds_json = f.read()
@@ -62,28 +51,48 @@ def get_gcp_credentials() -> str:
         logger.error(f'GCP credentials were not found with error: {e}')
 
 
-
-
 def test_fetch_regions():
     gcp_credentials_json = get_gcp_credentials()
-    # if not os.path.exists(GCP_CREDENTIALS_DIRECTORY):
-    #     logger.info(f'{GCP_CREDENTIALS_DIRECTORY} was not found. Creating a directory')
-    #     os.makedirs(GCP_CREDENTIALS_DIRECTORY)
-    # if os.path.exists(GCP_CREDENTIALS_DIRECTORY):
-    #     logger.info(f'{GCP_CREDENTIALS_DIRECTORY} exists, removing the file')
-    #     os.remove(GCP_CREDENTIALS_PATH)
-    # f = open(GCP_CREDENTIALS_PATH, 'a')
-    # f.write(gcp_credentials_json)
-    # f.close()
-    # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = GCP_CREDENTIALS_PATH
-    print(gcp_credentials_json)
     gcp_project_id = json.loads(gcp_credentials_json)['project_id']
     regions_list = fetch_regions(gcp_project_id)
     assert isinstance(regions_list, list)
     assert len(regions_list) > 0
 
-# def test_fetch_zones():
-#     pass
-#
-# def test_fetch_subnets():
-#     pass
+
+def test_fetch_zones():
+    gcp_credentials_json = get_gcp_credentials()
+    gcp_project_id = json.loads(gcp_credentials_json)['project_id']
+    zones_list = fetch_zones(gcp_project_id)
+    assert isinstance(zones_list, list)
+    assert len(zones_list) > 0
+    pass
+
+
+def test_fetch_kubernetes_versions():
+    try:
+        credentials = service_account.Credentials.from_service_account_file(
+            GCP_CREDENTIALS_PATH)
+        service = discovery.build('container', 'v1', credentials=credentials)
+        gcp_credentials_json = get_gcp_credentials()
+        gcp_project_id = json.loads(gcp_credentials_json)['project_id']
+        zones_list = fetch_zones(gcp_project_id)
+        kubernetes_versions_dict = fetch_kubernetes_versions(zones_list, gcp_project_id, service)
+        assert isinstance(kubernetes_versions_dict, dict)
+        assert len(kubernetes_versions_dict) > 0
+    except Exception as e:
+        logger.error(f'Credentials were not provided with a file with error: {e}')
+
+def test_fetch_gke_image_types():
+    try:
+        credentials = service_account.Credentials.from_service_account_file(
+            GCP_CREDENTIALS_PATH)
+        service = discovery.build('container', 'v1', credentials=credentials)
+        gcp_credentials_json = get_gcp_credentials()
+        gcp_project_id = json.loads(gcp_credentials_json)['project_id']
+        zones_list = fetch_zones(gcp_project_id)
+        gcp_project_id = json.loads(gcp_credentials_json)['project_id']
+        gke_image_types_list = fetch_gke_image_types(zones_list, gcp_project_id, service)
+        assert isinstance(gke_image_types_list, list)
+        assert len(gke_image_types_list) > 0
+    except Exception as e:
+        logger.error(f'Credentials were not provided with a file with error: {e}')
